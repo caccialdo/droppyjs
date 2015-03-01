409adff9ca8b0aa9b294e56e4a98e1d9f7a009a8
caccialdo <caccialdo@gmail.com>
2015-03-01 15:22:44 +0000

    better name for browser support function

--- src/utils.js
+++ src/utils.js
@@ -3,19 +3,19 @@ function bindEvent(node, type, handler, context) {
     node.addEventListener(type, handler);
     return {
         detach: node.removeEventListener.bind(node, type, handler)
-    }
+    };
 }
 
 /**
  * Original code from Chosen
  * cf. https://github.com/harvesthq/chosen/blob/master/coffee/lib/abstract-chosen.coffee#L260
  */
-function browserIsNotSupported (navigator) {
+function isBrowserSupported(navigator) {
     var isIE = navigator.appName === "Microsoft Internet Explorer" && document.documentMode < 9,
         isIOS = /iP(od|hone)/i.test(navigator.userAgent),
         isAndroid = /Android/i.test(navigator.userAgent);
 
-    return isIE || isIOS || isAndroid;
+    return !isIE && !isIOS && !isAndroid;
 }
 
 function shallowMerge (o1, o2) {
